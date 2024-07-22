a72499a918c00da7b625491e793c33ceee4acc53
Sudhin05 <sudhindrapai11@gmail.com>
2024-07-22 16:43:25 +0530

    Added HeroSection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import OffcanvasExample from './components/Navbar/Navbar';
+import ControlledCarousel from './components/Carousel/Carousel';
 
 function App() {
   return (
     <div>
     <OffcanvasExample/>
+    <ControlledCarousel/>
     </div>
   );
 }
